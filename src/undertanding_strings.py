f93885ca94406e35c4b7466acbae6ea9f3d68004
2530493 <2530493@upv.edu.mx>
2025-10-30 17:05:20 -0600

    featured4

--- src/undertanding_strings.py
+++ src/undertanding_strings.py
@@ -8,3 +8,29 @@ Ejemplos:
 ' Le dije a un amigo "Python es mi lenguaje favorito" '
 "El lenguaje 'Python' lleva el nombre por Monty Python, no por la serpiente"
 """
+#Whitespaces
+"""
+|  Un whitespace se refiere a cualquier caracter que no se imprime, es decir 
+|  espacio o tabulador Y finales de línea los Bowl Space se utilizan complemente 
+|  para organizar la salida de tal manera que sea más amigable de leer o ver para 
+|  el usuario.
+| Ejemplo:
+|  + Tabuladore: \t
+|  + Salto de linea: \n
+"""
+print("whitesaces tabulador")
+print("Python")
+print("\tPython")
+print("\t\tPython")
+
+print("whitespaces salto de linea")
+print("languuages: \nPython\cd:\nJavaScrips")
+python01 =  " Python "
+print(python01)
+print(python01.rstrip())
+print(python01.lstrip())
+print(python01.strip())
+
+# Syntax Error con strings
+message = 'Una fortaleza de python es su comunidad' 
+print(message)
