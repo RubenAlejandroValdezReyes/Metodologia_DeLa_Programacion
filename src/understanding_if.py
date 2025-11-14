83e95c8831849b5b5c444e635ffd2aeccab0ed34
2530493 <2530493@upv.edu.mx>
2025-11-14 07:52:40 -0600

    featured17

--- src/understanding_if.py
+++ src/understanding_if.py
@@ -36,3 +36,50 @@ print(age > 21)  # Mayor que
 print(age >= 21) # Mayor o igual a
 
 # Multiples condiciones
+age_0 = 22
+age_1 = 18
+print("Multiples condiciones:")
+print("Operación AND:")
+print(age_0 >= 21 and age_1 >= 21)  # Ambas condiciones deben ser verdaderas
+print(age_0 >= 21 and age_1 >= 18)
+
+print("Operación OR:")
+print(age_0 >= 21 or age_1 >= 21)   # Al menos una condición debe ser verdadera
+print(age_0 >= 21 or age_1 >= 18)
+
+# Como nos preguntan si un valor está en una lista
+requested_toppings = ["mushrooms", "onions", "pineapple"]
+print("mushrooms" in requested_toppings)  # True    
+print("pepperoni" in requested_toppings)  # False
+
+# A value no está en una lista
+banned_users = ["andrew", "carolina", "david, max, gabriel, quevedo"]
+user = "marie"
+print(user not in banned_users)  # Truedad
+
+# Variables de tipo booleano
+game_active = True
+can_edit = False
+
+"""
+    If stantements
+    
+    If condition:
+        Do something
+
+    IF condition:
+        So something True
+
+        So something false
+"""
+#Preguntar edad del usuario
+#Y decirle si tiene la edad
+#Suficiente ara votar.
+#Suficiente para vothar 
+#imput(**) --> str
+age = int(input("\nPlease enter your age: "))
+print/f"Your age is: {age}")
+
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")           
