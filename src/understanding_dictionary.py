80b40cc48c6e126b3a265b1b7d97b25b87cdc647
2530493 <2530493@upv.edu.mx>
2025-11-26 09:51:02 -0600

    Featured20

--- src/understanding_dictionary.py
+++ src/understanding_dictionary.py
@@ -1,7 +1,65 @@
 # H
-homer_0 = {}
+homer_0 = {"color": "yellow", "bag": "maggie_bag"}
 print(homer_0)
 print(type(homer_0))
-marge = {"color": "yellow", "bag": "homer_chino", "headshot": 1.5}
+marge = {"color": "yellow", "bag": "homer_chino", "headshot": 1.5, "hair": "blue", "dress": "green"}
+gun = {"color": "yellow-orange", "headshot": 1.5}
 
 print(marge["bag"])
+
+covenant_grounts = {
+    "color": "orange",
+    "weapon": "plasma_gun",
+    "armament": "plasma_granade",
+    "health": 2,
+
+}
+covenant_Elite = {
+    "color": "blue",
+    "weapon": "energy_sword",
+    "armament": "plasma_granade",
+    "health": 7,
+}
+covenant_jackal = {
+    "color": "gray",
+    "weapon": "plasma_gun",
+    "armament": "plasma_granade",
+    "health": 5,
+}
+covenants = [
+    covenant_grounts,
+    covenant_Elite, 
+    covenant_jackal
+]
+
+for covenant in covenants:
+    print("\n", covenant)
+    for key, value in covenant.items():
+        print(key, value)
+
+print("\n")
+students = {
+    "santiago": ["reprobado", "prepal", "rebelde"],
+    "jorge-crack ": ["aprobado", "cbtis271", "irrespetuoso"],
+    "gabriel": ["aprobado", "119muerte", "fornite"]
+}
+
+##Diccionarios en diccionarios
+sensors = {
+    "temperature": {
+        "value": 23,
+        "location": "aula 105"
+        "id": "T-1000"
+        "unit": "Celsius"
+        },  
+    "humidity": {
+        "value": 60,
+        "location": "aula 105"
+        "id": "H-2000"
+        "unit": "%"
+        },
+}
+print("Temperatura")
+print(sensors["temperature"]["value"])
+print   
+print(sensors["humidity"]["location"])
---
+++ src/understanding_while_loop.py
@@ -0,0 +1,18 @@
+#While
+"""
+El while es un ciclo controlado/comando por condición.
+La estructura básica es:
+while condición:
+    action
+"""
+#While infinito
+while True:
+    print("Este ciclo es infinito")
+    
+while true:
+    try  number = int(input("Ingresa un número mayor a 100: "))
+    if number >= 25 and number  <= 50:
+        
+    else:
+        print("Felicidades, ingresaste un número mayor a 100")
+        break
